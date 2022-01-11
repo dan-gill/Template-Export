49c452b7ebc6565e42dd3f1cfbfd6f67419fc8ac
Dan Gill <dan.gill@dgillconsulting.com>
2022-01-11 15:28:52 -0300

    :wrench: Convert to JSON settings file
    Convert code to use JSON settings file

--- Export-Templates.ps1
+++ Export-Templates.ps1
@@ -9,24 +9,13 @@
 #######################
 # Specify a directory to download and install the new PowerCLI module to for future offline access
 $PSModulePath = "$Env:SystemDrive\PowerCLIModule\"
-# Import settings from settings.txt file
-Get-Content -Path "$PSScriptRoot\settings.txt" |
-ForEach-Object -Begin {
-    # Create Hashtable
-    $Settings = @{}
-} -Process {
-    # Retrieve line with '=' and split them
-    $k = [regex]::Split($_, '=')
-    if (($k[0].CompareTo('') -ne 0) -and ($k[0].StartsWith('[') -ne $True)) {
-        # Add the Key, Value into the Hashtable
-        $Settings.Add(($k[0] -replace '\s+', ''), ($k[1] -replace '\s+', ''))
-    }
-}
-$MaxRunspaces = 20
+# Import settings from settings.json file
+$Settings = Get-Content "$PSScriptRoot\settings.json" -Raw | ConvertFrom-Json
+$MaxRunspaces = $Settings.General.MaxRunspaces
 # Ohio vCenters
-$vCentersOH = 'OHOS-VDI-VCS107.opsus.local', 'OHOS-VC01.opsus.local'
+$vCentersOH = $Settings.vCenters.vCentersOH
 # Texas vCenters
-$vCentersTX = 'TXOS-VDI-VCS104.opsus.local', 'TXOS-VC01.opsus.local'
+$vCentersTX = $Settings.vCenters.vCentersTX
 # Synchronized hashtable
 $Configuration = [hashtable]::Synchronized(@{})
 $Configuration.ScriptErrors = @()
@@ -36,155 +25,157 @@ if (Test-Path -Path $SavePath) { Remove-Item -Path "$SavePath" -Recurse -Force }
 $null = New-Item -Path "$SavePath" -ItemType Directory -Force
 $ScriptErrors = "$SavePath\$(Get-Date -f yyyy-MM-dd)_ScriptErrors.log"
 # DataDomain password
-$password = ConvertTo-SecureString $Settings.passwd -AsPlainText -Force
-$Cred = New-Object System.Management.Automation.PSCredential ($Settings.username, $password)
+$Cred = New-Object System.Management.Automation.PSCredential ($Settings.DataDomain.username, (ConvertTo-SecureString $Settings.DataDomain.passwd -AsPlainText -Force))
 # DataDomains
-$DataDomainOH = '\\ohos-dd6300-1\z_EUC_From_OH'
-$DataDomainTX = '\\txos-dd6300-1\z_EUC_From_TX'
+$DataDomainOH = $Settings.DataDomain.DataDomainOH
+$DataDomainTX = $Settings.DataDomain.DataDomainTX
 # Match any case of {3 to 4 character mneumonic}-view, {3 to 4 character mneumonic}-view-{1 character},
 # {3 to 4 character mneumonic}-ctx-appmx, or {3 to 4 character mneumonic}-ctx-appmx-{1 character}
 # Mneumonic must only contain letters from the English alphabet
 $regexPattern = '^[a-zA-Z]{3,4}-(?:[Vv][Ii][Ee][Ww]|[Cc][Tt][Xx]-[Aa][Pp][Pp][Mm][Xx]).*$'
 
 # Number of backups to retain
-$retain = 26
+$retain = $Settings.General.RetainedCopies
 
 # The script needs to run on an OH or TX Engineer Desktop
-if ($env:computername -match "^[Tt][Xx][Oo][Ss]-[Ee][Nn][Gg]\d{2}$") {
+if ($env:computername -match '^[Tt][Xx][Oo][Ss]-[Ee][Nn][Gg]\d{2}$') {
     # Local computer is in TX, only list TX vCenters
     $vCenters = $vCentersTX
-} elseif ($env:computername -match "^(?:[Oo][Hh][Oo][Ss]|[Oo][Pp][Ss][Uu][Ss])-[Ee][Nn][Gg]-\d{2}$") {
+} elseif ($env:computername -match '^(?:[Oo][Hh][Oo][Ss]|[Oo][Pp][Ss][Uu][Ss])-[Ee][Nn][Gg]-\d{2}$') {
     # Local computer is in OH, only list OH vCenters
     $vCenters = $vCentersOH
 } else {
     Write-Error -Message "You must run this script from a VDI in OH or TX.`r`nExamples: OPSUS-ENG-12, OHOS-ENG-35, or TXOS-ENG97" -Category PermissionDenied
     Exit 10              # Exiting script
 }
+
 #####################################################################
 # Nothing to change below this line, commented throughout to explain
 #####################################################################
-#######################
-# Testing if TLS 1.2 or above is enabled
-#######################
-if ([Net.ServicePointManager]::SecurityProtocol -notlike '*Tls1[23]*') {
-    #######################
-    # Set Strong Cryptography - https://docs.microsoft.com/en-us/officeonlineserver/enable-tls-1-1-and-tls-1-2-support-in-office-online-server#enable-strong-cryptography-in-net-framework-45-or-higher
-    #######################
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
-    Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
-    #######################
-    # Disable TLS 1.0 and 1.1
-    #######################
-    if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client') {
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
-    }
-    if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server') {
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
-    }
-    if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client') {
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
-    }
-    if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server') {
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
-        Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
-    }
-    #######################
-    # Exit and ask user to re-run the script
-    #######################
-    Write-Warning 'This system did not have TLS 1.2 or above enabled. Please re-run the script so that the changes to enable TLS 1.2 and above take effect.'
-    Exit
-}
-  
-#######################
-# Testing if PS Module path exists, if not creating it
-#######################
-$PSModulePathTest = Test-Path $PSModulePath
-if ($PSModulePathTest -eq $False) {
-    New-Item -ItemType Directory -Force -Path $PSModulePath
-}
-#######################
-# Checking to see if PowerCLI is installed in Program Files, takes 5-30 seconds
-#######################
-Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -CurrentOperation 'Checking if PowerCLI is installed in Program Files, wait 5-30 seconds'
-$PowerCLIInstalled = Get-WmiObject -Class Win32_Product -Filter "Name='VMware vSphere PowerCLI'"
-#######################
-# If PowerCLI is installed then removing it, so we can run from the module instead
-#######################
-if ($PowerCLIInstalled) {
-    Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -CurrentOperation 'Uninstalling to allow for new PowerCLI module'
-    # Uninstalling PS module
-    $PowerCLIUninstall = $PowerCLIInstalled.Uninstall()
-    # Checking return value for success
-    $PowerCLIUninstallValue = $PowerCLIUninstall.ReturnValue
-    if ($PowerCLIUninstallValue -ne 0) {
-        Write-Error -Message 'Uninstall Of PowerCLI Failed - Most likely due to not running as administrator' -Category PermissionDenied
-    }
-    # Finished uninstall
-    Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -Completed
-}
-Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -Completed
-#######################
-# Checking to see if the NuGet Package Provider is already installed
-#######################
-$NuGetPackageProviderCheck = Get-PackageProvider -Name 'NuGet' -ListAvailable
-#######################
-# If NuGet Provider is not installed, nothing found, then running install...
-#######################
-if (!$NuGetPackageProviderCheck) {
-    Write-Progress -Id 1 -Activity 'NuGet Package Provider' -CurrentOperation 'Not Found - Installing'
-    # Trusting PS Gallery to remove prompt on install
-    Set-PackageSource -Name 'PSGallery' -Trusted
-    # Not installed, finding module online
-    Find-PackageProvider -Name 'NuGet' -AllVersions
-    # Installing module
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
-    Write-Progress -Id 1 -Activity 'NuGet Package Provider' -Completed
-}
-#######################
-# Checking to see if the PowerCLI module is already installed
-#######################
-$PowerCLIModuleCheck = Get-Module -ListAvailable -Name 'VMware.PowerCLI'
-#######################
-# If PowerCLI module is not installed, nothing found, then running install...
-#######################
-if (!$PowerCLIModuleCheck) {
-    Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Not Found - Installing'
-    # Trusting PS Gallery to remove prompt on install
-    Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
-    # Not installed, finding module online
-    Find-Module -Name 'VMware.PowerCLI'
-    # Installing module
-    Install-Module -Name 'VMware.PowerCLI' -Confirm:$false -AllowClobber -Force
-    # If running this is a repeat demo/test, you can uninstall the module using the below:
-    # Uninstall-Module -Name VMware.PowerCLI -Confirm:$false
-    Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
-}
-#######################
-# Testing import of PowerCLI module
-#######################
-Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Importing'
-$null = Import-Module -Name 'VMware.PowerCLI'
-Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
-Try {
-    $null = Get-VICommand
-    $PowerCLIImportTest = $True
-} Catch {
-    $PowerCLIImportTest = $False
-} Finally {
-    #######################
-    # Outputting result
-    #######################
-    if ($PowerCLIImportTest) {
-        Write-Information 'New PowerCLI Module Successfully Installed'
-    } else {
-        Write-Error -Message "Something went wrong! Maybe you, maybe me. Does this computer have internet access and did you run as administrator?`r`nTry installing PowerCLI in offline mode (Procedure 3): https://tinyurl.com/VMware-PowerCLI"
-        Exit 22
-    }
-}
 
+if (!(Get-Module -ListAvailable -Name VMware.PowerCLI)) {
+    #######################
+    # Testing if TLS 1.2 or above is enabled
+    #######################
+    if ([Net.ServicePointManager]::SecurityProtocol -notlike '*Tls1[23]*') {
+        #######################
+        # Set Strong Cryptography - https://docs.microsoft.com/en-us/officeonlineserver/enable-tls-1-1-and-tls-1-2-support-in-office-online-server#enable-strong-cryptography-in-net-framework-45-or-higher
+        #######################
+        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
+        Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\.NetFramework\v4.0.30319' -Name 'SchUseStrongCrypto' -Value '1' -Type DWord -Force -Confirm:$false
+        #######################
+        # Disable TLS 1.0 and 1.1
+        #######################
+        if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client') {
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+        }
+        if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server') {
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+        }
+        if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client') {
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+        }
+        if (Test-Path -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server') {
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'DisabledByDefault' -Value '1' -Type DWord -Force -Confirm:$false
+            Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Name 'Enabled' -Value '0' -Type DWord -Force -Confirm:$false
+        }
+        #######################
+        # Exit and ask user to re-run the script
+        #######################
+        Write-Warning 'This system did not have TLS 1.2 or above enabled. Please re-run the script so that the changes to enable TLS 1.2 and above take effect.'
+        Exit
+    }
+  
+    #######################
+    # Testing if PS Module path exists, if not creating it
+    #######################
+    $PSModulePathTest = Test-Path $PSModulePath
+    if ($PSModulePathTest -eq $False) {
+        New-Item -ItemType Directory -Force -Path $PSModulePath
+    }
+    #######################
+    # Checking to see if PowerCLI is installed in Program Files, takes 5-30 seconds
+    #######################
+    Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -CurrentOperation 'Checking if PowerCLI is installed in Program Files, wait 5-30 seconds'
+    $PowerCLIInstalled = Get-WmiObject -Class Win32_Product -Filter "Name='VMware vSphere PowerCLI'"
+    #######################
+    # If PowerCLI is installed then removing it, so we can run from the module instead
+    #######################
+    if ($PowerCLIInstalled) {
+        Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -CurrentOperation 'Uninstalling to allow for new PowerCLI module'
+        # Uninstalling PS module
+        $PowerCLIUninstall = $PowerCLIInstalled.Uninstall()
+        # Checking return value for success
+        $PowerCLIUninstallValue = $PowerCLIUninstall.ReturnValue
+        if ($PowerCLIUninstallValue -ne 0) {
+            Write-Error -Message 'Uninstall Of PowerCLI Failed - Most likely due to not running as administrator' -Category PermissionDenied
+        }
+        # Finished uninstall
+        Write-Progress -Id 2 -ParentId 1 -Activity 'PowerCLI in Program Files' -Completed
+    }
+    Write-Progress -Id 1 -Activity 'Checking for PowerCLI' -Completed
+    #######################
+    # Checking to see if the NuGet Package Provider is already installed
+    #######################
+    $NuGetPackageProviderCheck = Get-PackageProvider -Name 'NuGet' -ListAvailable
+    #######################
+    # If NuGet Provider is not installed, nothing found, then running install...
+    #######################
+    if (!$NuGetPackageProviderCheck) {
+        Write-Progress -Id 1 -Activity 'NuGet Package Provider' -CurrentOperation 'Not Found - Installing'
+        # Trusting PS Gallery to remove prompt on install
+        Set-PackageSource -Name 'PSGallery' -Trusted
+        # Not installed, finding module online
+        Find-PackageProvider -Name 'NuGet' -AllVersions
+        # Installing module
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force -Confirm:$false
+        Write-Progress -Id 1 -Activity 'NuGet Package Provider' -Completed
+    }
+    #######################
+    # Checking to see if the PowerCLI module is already installed
+    #######################
+    $PowerCLIModuleCheck = Get-Module -ListAvailable -Name 'VMware.PowerCLI'
+    #######################
+    # If PowerCLI module is not installed, nothing found, then running install...
+    #######################
+    if (!$PowerCLIModuleCheck) {
+        Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Not Found - Installing'
+        # Trusting PS Gallery to remove prompt on install
+        Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
+        # Not installed, finding module online
+        Find-Module -Name 'VMware.PowerCLI'
+        # Installing module
+        Install-Module -Name 'VMware.PowerCLI' -Confirm:$false -AllowClobber -Force
+        # If running this is a repeat demo/test, you can uninstall the module using the below:
+        # Uninstall-Module -Name VMware.PowerCLI -Confirm:$false
+        Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
+    }
+    #######################
+    # Testing import of PowerCLI module
+    #######################
+    Write-Progress -Id 1 -Activity 'PowerCLI Module' -CurrentOperation 'Importing'
+    $null = Import-Module -Name 'VMware.PowerCLI'
+    Write-Progress -Id 1 -Activity 'PowerCLI Module' -Completed
+    Try {
+        $null = Get-VICommand
+        $PowerCLIImportTest = $True
+    } Catch {
+        $PowerCLIImportTest = $False
+    } Finally {
+        #######################
+        # Outputting result
+        #######################
+        if ($PowerCLIImportTest) {
+            Write-Information 'New PowerCLI Module Successfully Installed'
+        } else {
+            Write-Error -Message "Something went wrong! Maybe you, maybe me. Does this computer have internet access and did you run as administrator?`r`nTry installing PowerCLI in offline mode (Procedure 3): https://tinyurl.com/VMware-PowerCLI"
+            Exit 22
+        }
+    }
+}
 # Warn if the Certificate is invalid, but continue
 $null = Set-PowerCLIConfiguration -InvalidCertificateAction Warn -Scope Session -Confirm:$false
 
